395899a09bc4bd3a4e82502d5cf37e5c90bb6b1b
svetlanakorch <svetakorchagina@gamil.com>
2021-02-03 13:20:31 +0100

    Initial commit

---
+++ Assigment_1.py
@@ -0,0 +1,13 @@
+#Coursera Capstone Project Week1
+#Create Jupyter Notebook for Capstone project work
+
+#Week 1 - Assignment Details
+#Write some markdown to explain that this notebook will be mainly used for the capstone project.
+#Import the pandas library as pd.
+#Import the Numpy library as np.
+#Print the following the statement: Hello Capstone Project Course!
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
